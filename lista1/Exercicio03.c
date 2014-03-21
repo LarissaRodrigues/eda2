4b1085abcff482a11cf0c3ae67196bef30bca60a
Larissa <larissax55@gmail.com>
2014-03-20 23:43:52 -0300

    Exercício_3 atualizado

--- lista1/Exercicio03.c
+++ lista1/Exercicio03.c
@@ -1,12 +1,5 @@
-//Modifique o código da busca binária, trocando o cálculo dos índices inferiores e superiores de modo a obter uma busca por interpolação. 
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
-//const int size_table = 10;
-
-
 int main(){
 	
    int vetor[10],cont;
@@ -21,25 +14,20 @@ int main(){
 
     printf("\n Informe qual o valor deseja encontrar no vetor.");
     scanf("%d", &x);
-    
-    //  meio = (inf+sup)/2;
-    meio = (inf +(sup - inf))*((x- vetor[inf])/(vetor[sup]-vetor[inf]));
-	   
+   	meio = inf +((sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]));
     for(aux=0; aux<10; aux++){
       if(meio == vetor[aux]){
       	printf("O valor foi achado em %d", aux+1);
       }
       if(meio > x){
       	sup=meio-1;
-      	 meio = inf +(sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]);
+      	 meio = inf +((sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]));
       }
       if(meio < x){
-        inf= meio-1;
+        inf= meio+1;
         meio = inf +(sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]);
       }
 
     }
-    
-	system("pause");
 	return 0;
 }
