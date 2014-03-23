148a71ffc90fcc9073c28a4be4b5b4af168c2dae
Larissa <larissax55@gmail.com>
2014-03-23 13:58:25 -0300

    alteracoes na questao 03

--- lista1/Exercicio03.c
+++ lista1/Exercicio03.c
@@ -1,35 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define size_vector 100
+ 
+ 
 int main(){
 	
-   int vetor[10],cont;
+   int vector[size_vector],count;
    int inf = 1;
    int sup = 10;
-   int meio, aux, x;
-	
-   for(cont=0; cont<10; cont++){
-   	 vetor[cont]= cont;
-   	 printf("\n %d", vetor[cont]);
+   int meio, aux, x, op;
+   
+   do{
+ 
+   for(count=0; count<size_vector; count++){
+   	 vector[count]= count;
+   	 printf("\n %d", vector[count]);
    }
 
     printf("\n Informe qual o valor deseja encontrar no vetor.");
     scanf("%d", &x);
     
-   	meio = inf +((sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]));
+   	meio = inf +((sup - inf)* (x- vector[inf])/(vector[sup]-vector[inf]));
    	
-    for(aux=0; aux<10; aux++){
-      if(meio == vetor[aux]){
+    for(aux=0; aux<size_vector; aux++){
+      if(meio == vector[aux]){
       	printf("O valor foi achado em %d", aux+1);
       }
       if(meio > x){
       	sup=meio-1;
-      	 meio = inf +((sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]));
+      	 meio = inf +((sup - inf)* (x- vector[inf])/(vector[sup]-vector[inf]));
       }
       if(meio < x){
         inf= meio+1;
-        meio = inf +(sup - inf)* (x- vetor[inf])/(vetor[sup]-vetor[inf]);
-      }
-
+        meio = inf +(sup - inf)* (x- vector[inf])/(vector[sup]-vector[inf]);
+      } 
     }
+     
+ printf(" \n Se deseja realizar outra busca digite 1. ");
+ scanf ("%d", &op);
+    } while(op == 1);
+
+    
+    
+
 	return 0;
 }
