02facc9ed5be5765302f03e10872c42c025edecd
Larissa <larissax55@gmail.com>
2014-03-31 19:37:16 -0300

    Renomeando a questao 03 e fazendo alterações necessarias./ Subindo o grafico

--- lista1/Busca_Interpolacao.cpp
+++ lista1/Busca_Interpolacao.cpp
@@ -1,43 +1,50 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define size_vector 1000
 #include <time.h> 
+#include <iostream>
+const int size_vector = 10;
+using namespace std;
  
 int main(){
 	
    int vector[size_vector],count;
    int less = 1;
    int upper = 10;
-   int middle, aux;
-   int value, option;
+   int middle;
+   int aux;
+   int value; 
+   int option;
    clock_t start, end;
-   float result_time, result;
+   float result_time;
+   float result;
    
    do{
  
-   for(count=0; count<size_vector; count++){
-   	 vector[count]= count;
-   	 printf("\n %d", vector[count]);
-   }
-
-    printf("\n Informe qual o valor deseja encontrar no vetor.");
-    scanf("%d", &value);
+     for(count=0; count<size_vector; count++){
+     	 vector[count]= count;
+   	     cout << " "<<vector[count] <<endl;
+    }
     
+    cout << "Informe um valor desejado"<<endl;
+    cin >> value; 
+     
    	middle = less +((upper - less)* (value- vector[less])/(vector[upper]-vector[less]));
+   	cout << "O valor do meio e :"<<middle<<endl; 
    	
    	start = clock();
    	
     for(aux=0; aux<size_vector; aux++){
       if(middle == vector[aux]){
-      	printf("O valor foi achado em %d", aux+1);
+      	cout << "O valor desejado foi achado na posicao"<<aux+1<<endl;
       }
       if(middle > value){
       	upper = middle-1;
       	middle = less +((upper - less)* (value- vector[less])/(vector[upper]-vector[less]));
-      }
+        cout << "O valor do meio e :"<<middle<<endl;
+	  }
+      
       if(middle < value){
         less= middle + 1;
         middle = less +((upper - less)* (value- vector[less])/(vector[upper]-vector[less]));
+        cout << "O valor do meio e :"<<middle<<endl;
       } 
     }
  
@@ -46,10 +53,12 @@ int main(){
     result_time = end-start;
     result =  ((float)result_time)/CLOCKS_PER_SEC;
     
-    printf("\n O tempo utilizado para realizar a busca e %f", result );
-	 
-      printf(" \n Se deseja realizar outra busca digite 1. ");
-      scanf ("%d", &option);
+
+     cout << "O tempo utilizado para a busca foi"<<result<<endl;
+  
+     cout << "Se deseja continuar a execução digite 1"<<endl;
+     cin >> option;	 
+      
   } while(option == 1);
 
 	return 0;
--- lista1/busca_binaria.cpp
+++ lista1/busca_binaria.cpp
@@ -104,4 +104,6 @@ int main(){
       } /*}}}*/
     }
   }
+  system("pause");
+  return 0;
 }
